6569aadf1c5f4b8b2401c6ce818b764f3398e8f9
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-14 23:57:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,17 @@
 def countdown
   number = 10
-  number -= 1
+  until number == 0
   puts "#{number} SECOND(S)!"
-  
+  number-=1
 end
 end
 
   puts "HAPPY NEW YEAR!"
+  
+  def using_until
+   levitation_force = 6
+  until levitation_force == 10
+	puts "Wingardium Leviosa"
+	levitation_force+=1
+end
+end
